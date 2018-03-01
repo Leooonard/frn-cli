0606ce955fc2a5f55529500211ab3bdca73462a4
yiyangyao <yiyangyao@Ctrip.com>
2018-03-01 14:23:52 +0800

    [refactor] 将具体功能拆分至子模块

--- src/services/init.ts
+++ src/services/init.ts
@@ -1,30 +1,16 @@
 import * as Execa from 'execa';
 import * as Path from 'path';
-import * as fs from 'fs';
-import * as mkdirp from 'mkdirp';
-import Chalk from 'chalk';
 
-import showSpinner from '../util/spinner';
 import * as Log from '../util/log';
 import {
-    importConfig,
     EConfigType
 } from '../util/configManager';
-import {
-    ICommitizenConfig,
-    IDirConfig,
-    IFileConfig,
-    IHuskyConfig,
-    IJestConfig,
-    IDevDependencyConfig,
-    INpmDependencyConfig,
-    IGitDependencyConfig,
-    INpmScriptConfig
-} from '../util/configInterface';
 import createProject from '../util/createProject';
 import copyFiles from '../util/copyFiles';
 import mkdir from '../util/mkdir';
 import writeConfigToPackageJson from '../util/writeConfigToPackageJson';
+import installDependencies from '../util/installDependencies';
+import installDevDependencies from '../util/installDevDependencies';
 
 const TAOBAO_REGISTRY = 'https://registry.npm.taobao.org';
 
@@ -60,9 +46,9 @@ export default async function initProject(
         enterProject(projectName);
         copyFiles(configType, isOverride);
         mkdir(configType, isRedux);
-        await installDependencies(configType);
-        await installDevDependencies(configType);
-        writeConfigToPackageJson(configType);
+        await installDependencies(configType, isOverride);
+        await installDevDependencies(configType, isOverride);
+        writeConfigToPackageJson(configType, isOverride);
 
         if (isUseTaobaoRegistry) {
             await setNpmRegistry(originalRegistry);
@@ -100,92 +86,4 @@ function getLogLevel(isVerbose: boolean, isSilent: boolean): Log.ELogLevel {
 
 function enterProject(projectName: string) {
     process.chdir(Path.resolve(projectName));
-}
-
-async function installDependencies(configType: EConfigType) {
-    const npmDependenciesSpinner = showSpinner('正在安装npm依赖');
-
-    const npmDependenciesConfig = importConfig<INpmDependencyConfig>(configType, 'npmDependencies');
-    for (let i = 0 ; i < npmDependenciesConfig.length ; i++) {
-        const {
-            dependencyName,
-            version
-        } = npmDependenciesConfig[i];
-
-        let moduleName = '';
-        if (!version) {
-            moduleName = dependencyName;
-        } else {
-            moduleName = `${dependencyName}@${version}`;
-        }
-
-        try {
-            await Execa('npm', ['install', '--save', moduleName]);
-            Log.debug(`安装npm依赖${dependencyName}成功`);
-        } catch(e) {
-            npmDependenciesSpinner.hide();
-
-            const err: Error = e;
-            Log.error(err.message);
-            Log.error(err.stack);
-
-            process.exit(1);
-        }
-    }
-
-    npmDependenciesSpinner.hide();
-    Log.info('安装npm依赖成功');
-
-    const gitDependenciesSpinner = showSpinner('正在安装git依赖');
-
-    const gitDependenciesConfig = importConfig<IGitDependencyConfig>(configType, 'gitDependencies');
-    for (let i = 0 ; i < gitDependenciesConfig.length ; i++) {
-        const {
-            dependencyName,
-            gitUrl
-        } = gitDependenciesConfig[i];
-
-        try {
-            await Execa('npm', ['install', '--save', gitUrl]);
-            Log.debug(`安装git依赖${dependencyName}成功`);
-        } catch(e) {
-            gitDependenciesSpinner.hide();
-
-            const err: Error = e;
-            Log.error(err.message);
-            Log.error(err.stack);
-
-            process.exit(1);
-        }
-    }
-
-    gitDependenciesSpinner.hide();
-    Log.info('安装git依赖成功');
-}
-
-async function installDevDependencies(configType: EConfigType) {
-    const spinner = showSpinner('正在安装npm开发依赖');
-
-    const devDependenciesConfig = importConfig<IDevDependencyConfig>(configType, 'devDependencies');
-    for (let i = 0 ; i < devDependenciesConfig.length ; i++) {
-        const {
-            dependencyName
-        } = devDependenciesConfig[i];
-
-        try {
-            await Execa('npm', ['install', '--save-dev', dependencyName]);
-            Log.debug(`安装npm开发依赖${dependencyName}成功`);
-        } catch(e) {
-            spinner.hide();
-
-            const err: Error = e;
-            Log.error(err.message);
-            Log.error(err.stack)
-
-            process.exit(1);
-        }
-    }
-
-    spinner.hide();
-    Log.info('安装npm开发依赖成功');
 }
