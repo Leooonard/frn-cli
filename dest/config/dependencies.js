fae996c8675e6d535c61725f5f5f047825a7a70e
xx_ma <xx_ma@Ctrip.com>
2018-06-15 15:56:44 +0800

    不需要账号密码了

--- dest/config/dependencies.js
+++ dest/config/dependencies.js
@@ -2,6 +2,6 @@
 [
     {
         "dependencyName": "Flight-FunctionLibrary",
-        "gitUrl": "git+http://yiyangyao:AAbbcc123.@git.dev.sh.ctripcorp.com/FlightMobile/Flight-FunctionLibrary.git#master"
+        "gitUrl": "git+http://git.dev.sh.ctripcorp.com/FlightMobile/Flight-FunctionLibrary.git#master"
     }
 ];
